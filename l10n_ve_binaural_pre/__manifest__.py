feb834441a15dc9c767289dca2cf7c119f3acfc0
Gustavo Lopez <git.gustavolopez.xyz@gmail.com>
2025-05-21 14:53:27 -0400

    [FIX] l10n_ve_binaural_pre: elimina tipos de documentos que no deben generarse como asientos directamente

--- l10n_ve_binaural_pre/__manifest__.py
+++ l10n_ve_binaural_pre/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Odoo S.A., LIDALabs",
     "website": "https://lidalabs.com",
     "category": "Accounting/Localizations",
-    "version": "17.0.1.0.2",
+    "version": "17.0.1.0.3",
     "depends": [
         "account",
         "l10n_latam_invoice_document",
