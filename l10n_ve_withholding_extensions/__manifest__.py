17dee1570fc264a542a9adf03ff461868b1231c6
Gustavo Lopez <git.gustavolopez.xyz@gmail.com>
2025-06-06 16:53:11 -0400

    [ADD] l10n_ve_withholding_extensions: nuevo modulo que extiende a l10n_ve_payment_extension
    - oculta pestañas de retenciones en asientos contables donde no aplica

---
+++ l10n_ve_withholding_extensions/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Extensiones de Venezuela - Retenciones",
+    "summary": """Extiende el Módulo de Retenciones Venezuela""",
+    "author": "LIDALabs",
+    "website": "https://lidalabs.com",
+    "category": "Accountant/Accountant",
+    "version": "17.0.1.0.0",
+    "license": "LGPL-3",
+    "depends": ["account", "l10n_ve_payment_extension"],
+    "auto_install": True,
+    "application": False,
+    "data": [
+        "views/account_move_views.xml",
+    ],
+    "demo": [
+    ],
+}
