7c2f5a0528e8b43873b5559d0c58ca507ceb7460
Gustavo López <git.gustavolopez.xyz@gmail.com>
2025-06-23 19:30:28 -0400

    [IMP] l10n_ve_withholding_extensions: agrega filtro por fecha contable a busqueda de retention_line_report_views

--- l10n_ve_withholding_extensions/__manifest__.py
+++ l10n_ve_withholding_extensions/__manifest__.py
@@ -5,13 +5,14 @@
     "author": "LIDALabs",
     "website": "https://lidalabs.com",
     "category": "Accountant/Accountant",
-    "version": "17.0.1.1.0",
+    "version": "17.0.1.2.0",
     "license": "LGPL-3",
     "depends": ["account", "l10n_ve_payment_extension"],
     "auto_install": True,
     "application": False,
     "data": [
         "views/account_move_views.xml",
+        "views/retention_line_report_views.xml",
     ],
     "demo": [
     ],
