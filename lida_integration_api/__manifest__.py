098e2947376755efb5238a06073d3ed66f4cbbeb
AstroLui <luisdaniel16an@gmail.com>
2026-01-14 20:22:59 -0400

    [FIX]: Cambiando la ubicacion a un nuevo modulo

---
+++ lida_integration_api/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "LIDA Integration API",
+
+    'summary': "Endpoints",
+
+    'description': """""",
+
+    'author': "LIDALabs",
+    'website': "https://lidalabs.com",
+    'category': 'API',
+    'version': '0.1.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+    ],
+    # Icon
+    "images": ["static/description/l10n_ve.png"],
+}
+
