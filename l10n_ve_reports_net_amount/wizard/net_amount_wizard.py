1347847200af6aa646025cb5a60bda9358d79a27
AstroLui <luisdaniel16an@gmail.com>
2026-02-08 20:53:22 -0600

    [FEAT]: Agregando un valor default a los campos de "desde" y "hasta" del wizard

--- l10n_ve_reports_net_amount/wizard/net_amount_wizard.py
+++ l10n_ve_reports_net_amount/wizard/net_amount_wizard.py
@@ -1,19 +1,34 @@
 
 from odoo import fields, api, models, _
 from odoo.tools.float_utils import float_round
+from dateutil.relativedelta import relativedelta
 
 class NetAmountWizard(models.TransientModel): 
     _name = "net.amount.wizard"
     _description = "Asistente de reportes de las cantidades netas"
+  
+    # ------------------------------    
+    # Valores por defectos de las fechas
+    # Son los ultioms 30 dias
+    def _get_default_date_from(self):
+        # ultimos 30 dias
+        return ( fields.Datetime.now() - relativedelta(months=1) )
+        ...
 
+    def _get_defualt_date_to(self): 
+        # dia de hoy
+        return fields.Datetime.now()
+        ...
+    # ------------------------------
+        
     product_id = fields.Many2one(
         'product.product',
         "Producto",
         required=True,
     )
 
-    date_from = fields.Date('Desde', required=True)
-    date_to = fields.Date('Hasta', required=True)
+    date_from = fields.Date('Desde', default=_get_default_date_from, required=True)
+    date_to = fields.Date('Hasta', default=_get_defualt_date_to, required=True)
 
 
     def action_sent_result(self): 
@@ -24,7 +39,7 @@ class NetAmountWizard(models.TransientModel):
             'type': 'ir.actions.act_window',
             'res_model': 'net.amount.result',
             'view_mode': 'form',
-            'target': 'new',
+            'target': 'current',
             'context': {
                 'default_product_id': self.product_id.id,
                 'default_date_from': self.date_from,
