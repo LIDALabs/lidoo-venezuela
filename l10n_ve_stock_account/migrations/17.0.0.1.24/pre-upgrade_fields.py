5637c20e779bf09c1fe45df01d47bd71744f938b
Gustavo López <git.gustavolopez.xyz@gmail.com>
2026-02-13 00:00:52 -0400

    [FIX] l10n_ve_invoice_extensions: arregla update script

--- l10n_ve_stock_account/migrations/17.0.0.1.24/pre-upgrade_fields.py
+++ l10n_ve_stock_account/migrations/17.0.0.1.24/pre-upgrade_fields.py
@@ -1,6 +1,7 @@
 
 import logging
 
+from odoo.tools.sql import column_exists
 from odoo.upgrade import util
 
 _logger = logging.getLogger(__name__)
@@ -8,4 +9,6 @@ _logger = logging.getLogger(__name__)
 
 def migrate(cr, version):
     _logger.info("Updating 'picking_ids'")
+    if not column_exists(cr, 'account_move', 'picking_id'):
+        return
     util.convert_m2o_field_to_m2m(cr, 'account.move', 'picking_id', 'picking_ids', 'pickings_invoice_rel', 'account_move_id', 'stock_picking_id')
