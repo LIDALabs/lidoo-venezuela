2719fce08795187b99fb025a2cbdfba1183c0a65
Gustavo López <git.gustavolopez.xyz@gmail.com>
2026-02-19 13:25:33 -0400

    [IMP] stock_account: mejora script de actualización

--- l10n_ve_stock_account/migrations/17.0.0.1.24/pre-upgrade_fields.py
+++ l10n_ve_stock_account/migrations/17.0.0.1.24/pre-upgrade_fields.py
@@ -2,7 +2,6 @@
 import logging
 
 from odoo.tools.sql import column_exists
-from odoo.upgrade import util
 
 _logger = logging.getLogger(__name__)
 
@@ -11,4 +10,5 @@ def migrate(cr, version):
     _logger.info("Updating 'picking_ids'")
     if not column_exists(cr, 'account_move', 'picking_id'):
         return
+    from odoo.upgrade import util
     util.convert_m2o_field_to_m2m(cr, 'account.move', 'picking_id', 'picking_ids', 'pickings_invoice_rel', 'account_move_id', 'stock_picking_id')
