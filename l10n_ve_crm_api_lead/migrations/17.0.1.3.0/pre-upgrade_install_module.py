283ad995e3962e95fad55ced1ac65dc60078656a
AstroLui <luisdaniel16an@gmail.com>
2026-02-20 12:11:11 -0600

    [FEAT]: Agrengando algunos _logger en las migraciones

--- l10n_ve_crm_api_lead/migrations/17.0.1.3.0/pre-upgrade_install_module.py
+++ l10n_ve_crm_api_lead/migrations/17.0.1.3.0/pre-upgrade_install_module.py
@@ -5,9 +5,10 @@ from odoo.upgrade import util
 _logger = logging.getLogger(__name__)
 
 def migrate(cr, version):
-    _logger.info("Verify if install CRM")
+    _logger.info("Verify if install CRM; if not installed, will do forced install")
     if not util.module_installed(cr, 'crm'):
         util.force_install_module(cr, 'crm', if_installed=None, reason="Es necesario este modulo para visualizar los leads generados")
+    _logger.info("Verify if install lida_api_auth; if not installed, will do forced install")
     if not util.module_installed(cr, 'lida_api_auth'):
         util.force_install_module(cr, 'lida_api_auth', if_installed=None, reason="Es necesario para proteger los endpoints")
     ...
