c0400d36b0b991ede0ff71a09124016200d44b48
AstroLui <luisdaniel16an@gmail.com>
2026-02-24 07:58:32 -0600

    [FEAT]: Agrengado el icono, titulo y descripcion

--- l10n_ve_rate_bcv_wizard/__manifest__.py
+++ l10n_ve_rate_bcv_wizard/__manifest__.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "l10n_ve_rate_bcv_wizard",
-    'summary': "Wizard to consult BCV rate and update product prices.",
-    'description': """""",
+    'name': "Asistente de Precios - Tasa BCV",
+    'summary': "Asistente para consultar la tasa del BCV y actualizar precios de productos",
+    'description': """
+    Es un wizard (asistente) que permite obtener el tipo de cambio oficial del Banco Central de Venezuela (BCV) y aplicar automáticamente esos valores para actualizar los precios de los productos en el sistema.
+    """,
+
     'author': "LIDALabs",
     'website': "https://lidalabs.com",
     'category': 'Accounting',
-    'version': '1.1.0',
+    'version': '17.0.1.1.0',
     'license': 'LGPL-3',
     'depends': ['l10n_ve_currency_rate_live', 'lida_reference_prices'],
     'data': [
@@ -14,6 +17,5 @@
         'wizard/bcv_rate_wizard_view.xml',
         'views/res_currency_views.xml',
     ],
-    'installable': True,
     "images": ["static/description/l10n_ve.png"],
 }
