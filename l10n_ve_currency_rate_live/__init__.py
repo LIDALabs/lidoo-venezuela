43e351dee08a8640ac77674e19efd18b57e57e55
MoisesLondo <moiseslondo16@gmail.com>
2026-02-24 23:18:29 -0400

    [FEAT]: Migrando wizard a currency live

--- l10n_ve_currency_rate_live/__init__.py
+++ l10n_ve_currency_rate_live/__init__.py
@@ -4,7 +4,18 @@ from dateutil.relativedelta import relativedelta
 from odoo import SUPERUSER_ID, api
 
 from . import models
+from . import wizard
 
+
+def _post_init_hook(env):
+    # Fix menu parenting for Enterprise (Contabilidad)
+    menu = env.ref('l10n_ve_currency_rate_live.menu_bcv_rate_wizard_account', raise_if_not_found=False)
+    ent_menu = env.ref('account_accountant.menu_accounting', raise_if_not_found=False)
+    if menu and ent_menu:
+        menu.parent_id = ent_menu
+    
+    # Existing setup logic
+    setup_currency_update(env)
 
 def setup_currency_update(env):
     env = api.Environment(env.cr, SUPERUSER_ID, {})
--- l10n_ve_currency_rate_live/__manifest__.py
+++ l10n_ve_currency_rate_live/__manifest__.py
@@ -6,10 +6,13 @@
     "website": "https://binauraldev.com/",
     "category": "Technical",
     "version": "17.0.1.1.1",
-    "depends": ["l10n_ve_rate", "currency_rate_live"],
+    "depends": ["l10n_ve_rate", "currency_rate_live", "lida_reference_prices"],
     "images": ["static/description/l10n_ve.png"],
     "data": [
+        "security/ir.model.access.csv",
+        "wizard/bcv_rate_wizard_view.xml",
         "views/res_config_settings.xml",
+        "views/res_currency_views.xml",
     ],
-    "post_init_hook": "setup_currency_update",
+    "post_init_hook": "_post_init_hook",
 }
