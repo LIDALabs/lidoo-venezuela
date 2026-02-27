838e120459bdab9b7db3135346adf46a033935a2
AstroLui <luisdaniel16an@gmail.com>
2026-02-27 10:35:54 -0600

    [FIX]: Cambiando el lugar donde se muestra el modulo al Top header del modulo "Contabilidad"

--- l10n_ve_currency_rate_live/__manifest__.py
+++ l10n_ve_currency_rate_live/__manifest__.py
@@ -6,7 +6,7 @@
     "website": "https://binauraldev.com/",
     "category": "Technical",
     "version": "17.0.1.1.1",
-    "depends": ["l10n_ve_rate", "currency_rate_live", "lida_reference_prices"],
+    "depends": ["l10n_ve_rate", "currency_rate_live", "lida_reference_prices", 'account_accountant'],
     "images": ["static/description/l10n_ve.png"],
     "data": [
         "security/ir.model.access.csv",
