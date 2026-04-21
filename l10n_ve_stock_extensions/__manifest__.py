46f4d9410bd560794c85c159adbee1dffd2b8db6
MoisesLondo <142282276+MoisesLondo@users.noreply.github.com>
2026-04-21 02:18:05 -0400

    [ADD] l10n_ve_stock_extensions: initial implementation and validate button fix

---
+++ l10n_ve_stock_extensions/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Venezuela - Stock Extensions",
+    "summary": "Módulo para extensiones de inventario de LIDALabs para Venezuela",
+    "version": "17.0.1.0.0",
+    "author": "LIDALabs",
+    "website": "https://lidalabs.com",
+    "category": "Inventory/Inventory",
+    "license": "LGPL-3",
+    "depends": ["stock", "l10n_ve_stock", "l10n_ve_stock_account"],
+    "data": [
+        "views/stock_picking_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
