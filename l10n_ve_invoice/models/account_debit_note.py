3e4cbb2f86f87c22ac0c34fdd0d9fd95d59d3d0f
MoisesLondo <moiseslondo16@gmail.com>
2026-07-15 10:39:40 -0400

    [FIX] l10n_ve_invoice: inherit payment terms when creating debit notes

--- l10n_ve_invoice/models/account_debit_note.py
+++ l10n_ve_invoice/models/account_debit_note.py
@@ -11,3 +11,9 @@ class AccountDebitNote(models.TransientModel):
         config = move.company_id.auto_select_debit_note_journal
         for record in self:
             record.filter_enabled = config
+
+    def _prepare_default_values(self, move):
+        """Hereda los términos de pago de la factura origen al crear la nota de débito."""
+        default_values = super()._prepare_default_values(move)
+        default_values['invoice_payment_term_id'] = move.invoice_payment_term_id.id
+        return default_values
